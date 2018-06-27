adca7c521f5771d88e4034c6a655522680dffb55
Dave Roberson <davidroberson@users.noreply.github.com>
2018-06-27 11:38:19 -0400

    Create torrent_server_html_block.R

---
+++ inst/extdata/ion_report/torrent_server_html_block.R
@@ -0,0 +1,51 @@
+#' ---
+#' title: TypeSeqer HPV Typing Report
+#' author: " "
+#' date: "`r format(Sys.time(), '%d %B, %Y')`"
+#' output:
+#'  html_document
+#' ---
+
+
+#+ load packages, echo=FALSE, include = FALSE
+library(tidyverse)
+library(stringr)
+library(jsonlite)
+library(scales)
+sessionInfo()
+
+
+#+ get args, echo=FALSE, include = FALSE
+read_lines("args.R") %>% 
+writeLines()
+source("args.R")
+
+
+#+ determine run type, echo=FALSE
+plugin_json = fromJSON(args_start_plugin_path, simplifyDataFrame = TRUE, simplifyMatrix = TRUE)
+
+run_type = plugin_json$runplugin$run_type
+
+#' # {.tabset}
+#' ## Analysis Output
+
+#+ full run, echo=FALSE, results='asis', eval=run_type!="thumbnail"
+
+cat('
+
+<a href="./TypeSeqer_QC_report.pdf" target="_blank">QC Report</a>
+  
+[TypeSeqer Report Files](./TypeSeqer_Report_Files.zip)  
+  
+
+  
+')
+
+#+ thumbnail run, echo=FALSE, results='asis', eval=run_type=="thumbnail"
+cat('
+
+Thumbnail data insufficient for typing analysis.  
+    
+Please see full report.
+
+')
