1e19f019826cdbedbb54d24683ff709352cdedf8
Dave Roberson <davidroberson@users.noreply.github.com>
2018-06-30 05:42:46 -0400

    Update illumina_read_processor_load_packages.R

--- R/illumina_read_processor_load_packages.R
+++ R/illumina_read_processor_load_packages.R
@@ -19,13 +19,25 @@ dynamic_require = function(x){
 }
 
 dynamic_require(c("tidyverse",
-                  "jsonlite",
-                  "pander",
-                  "scales",
-                  "fuzzyjoin",
-                  "ggsci",
-                  "koRpus"
+                  "jsonlite"
                   ))
+  
+dynamic_require_bioc = function(x){
+  for( i in x ){
+    #  require returns TRUE invisibly if it was able to load package
+    if( ! require( i , character.only = TRUE ) ){
+      #  If package was not able to be loaded then re-install
+      source("https://bioconductor.org/biocLite.R")
+      biocLite(i)
+      install.packages( i , dependencies = TRUE )
+      #  Load package after installing
+      require( i , character.only = TRUE )
+    }
+  }
+}
+
+dynamic_require_bioc("GenomicAlignments")  
+  
 sessionInfo()
   
 }
