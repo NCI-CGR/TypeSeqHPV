8babfd7f79bbf942fb7a72b924c1a7d59ac740e9
Dave Roberson <davidroberson@users.noreply.github.com>
2018-07-11 13:58:05 -0400

    removed bc1_id

--- R/finalize_pn_matrix.R
+++ R/finalize_pn_matrix.R
@@ -6,7 +6,7 @@ left_join(control_results) %>%
 mutate(control_result = if_else(is.na(control_result), "sample", control_result)) %>%
 select(-Control_Code) %>%
 distinct() %>%
-select(-Num_Types_Pos, -control_result, -Human_Control, -starts_with("HPV"), control_result, Num_Types_Pos, Human_Control, starts_with("HPV")) %>%
+select(-Num_Types_Pos, -control_result, -Human_Control, -bc1_id, -starts_with("HPV"), control_result, Num_Types_Pos, Human_Control, starts_with("HPV")) %>%
 arrange(Sort_Order) %>%
-mutate(Num_Types_Pos = ifelse(is.na(Num_Types_Pos), 0, Num_Types_Pos))
+mutate(Num_Types_Pos = ifelse(is.na(Num_Types_Pos), 0, Num_Types_Pos)) 
 }
