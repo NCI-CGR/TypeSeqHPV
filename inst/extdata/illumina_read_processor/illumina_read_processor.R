0bd3b61a157c29d683dac120e5dd9b69fb29d223
Dave Roberson <davidroberson@users.noreply.github.com>
2018-07-10 21:42:50 -0400

    Update illumina_read_processor.R

--- inst/extdata/illumina_read_processor/illumina_read_processor.R
+++ inst/extdata/illumina_read_processor/illumina_read_processor.R
@@ -9,12 +9,13 @@ require(sparklyr)
 config=spark_config()
 Sys.setenv("SPARK_MEM" = "200G")
 config$`sparklyr.shell.driver-memory` <- "200G"
-config$spark.executor.memory <- "7GB"
+#config$spark.executor.memory <- "7GB"
 config$sparklyr.cores.local <- "32"
-config$spark.executor.cores <- 2
+config$`sparklyr.shell.driver-cores` <- "32"
+#config$spark.executor.cores <- 2
 #config$`spark.cores.max` <- 32
-config$spark.dynamicAllocation.enabled <- TRUE  
-config$spark.shuffle.service.enabled <- TRUE
+#config$spark.dynamicAllocation.enabled <- TRUE  
+#config$spark.shuffle.service.enabled <- TRUE
 config$spark.memory.fraction <- 0.9
 
 sc <- spark_connect(master = "local", config = config, version = '2.3.0')
