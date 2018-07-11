684760a4ec59d0accce25128dc5aee3a18fa81fe
Dave Roberson <davidroberson@users.noreply.github.com>
2018-07-11 19:01:49 -0400

    Update lineage_plot.R

--- R/lineage_plot.R
+++ R/lineage_plot.R
@@ -42,7 +42,7 @@ geom_bar(stat="identity", position = "fill") +
 theme_light() +
 theme(
       axis.text.y = element_text(angle = 0, hjust = 1, color = "darkblue", size = 9),
-      axis.text.x = element_text(angle = 90, hjust = 0, size = 12, vjust = 0.5),
+      axis.text.x = element_text(angle = 90, hjust = 0, size = 18, vjust = 0.5),
       axis.line = element_line(colour = "darkblue",  size = 2, linetype = "solid")) +
 theme(strip.background = element_blank(),
       legend.position="bottom") +
