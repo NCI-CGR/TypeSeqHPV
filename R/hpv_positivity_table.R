b6ef3084e566398c3b6a84606c1df0bc3f7ace55
Dave Roberson <davidroberson@users.noreply.github.com>
2018-07-24 15:23:56 -0400

    Create hpv_positivity_table.R

---
+++ R/hpv_positivity_table.R
@@ -0,0 +1,30 @@
+#' HPV Positivity Table
+#'
+
+
+hpv_positivity_table <- function(split_deilverables){
+
+controls_df = split_deliverables$control_matrix %>%
+filter(Control_type == "neg") %>%
+group_by(Assay_Plate_Code, control_result) %>%
+summarize(count = n()) %>%
+bind_rows(data_frame(Assay_Plate_Code = "temp", control_result = c("pass", "fail"), count = c(0,0))) %>%
+spread(control_result, count, fill=0) %>%
+select(Assay_Plate_Code, num_neg_controls_passed = pass, num_neg_controls_failed=fail) 
+
+samples_and_controls_df = split_deliverables$samples_only_matrix %>%
+mutate(has_positive = ifelse(Num_Types_Pos == 0,0, 1)) %>%
+group_by(Assay_Plate_Code) %>%
+mutate(number_of_samples = n()) %>%
+mutate(hpv_pos_rate = sum(has_positive) / number_of_samples) %>%
+select(Assay_Plate_Code, hpv_pos_rate) %>%
+distinct() %>%
+mutate(hpv_pos_perc = scales::percent(hpv_pos_rate)) %>%
+select(-hpv_pos_rate) %>%
+left_join(controls_df)
+
+print(circlePlot)
+
+temp = temp
+
+}
