2b592ecb1e557df7819b087180b129f89028b9de
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-01 12:23:49 -0400

    Update create_full_run_read_metrics_df.R

--- R/create_full_run_read_metrics_df.R
+++ R/create_full_run_read_metrics_df.R
@@ -3,6 +3,8 @@ create_full_run_read_metrics_df <- function(read_metrics_path, hpv_type_counts){
   
 read_metrics = stream_in(file("read_metrics_merged.json")) %>%
 group_by(bc1_id, file_name) %>%
+#group_by(bc1_id, file_name) %>%
+
 do({
 df = .
 
@@ -16,7 +18,7 @@ distinct()
 }) %>%
 ungroup() %>%
 select(-page_num) %>%
-select(bc1_id, total_reads, mapq_greater_than_zero, pass_za, pass_seq_length, pass_mapq, file_name) %>%
+select(bc1_id, total_reads, mapq_greater_than_zero, pass_za, pass_seq_length, pass_mapq) %>%
 distinct() %>%
 left_join(hpv_type_counts %>% select(bc1_id, qualified_aligned_reads) %>% distinct()) %>%
 mutate(mapq_gtz_p = mapq_greater_than_zero / total_reads) %>%
@@ -24,7 +26,8 @@ mutate(pass_za_p = pass_za / total_reads) %>%
 mutate(pass_seq_length_p = pass_seq_length / total_reads) %>%
 mutate(pass_mapq_p = pass_mapq / total_reads) %>%
 mutate(qualified_p = qualified_aligned_reads / total_reads) %>%
-select(file_name, mapq_gtz_p, pass_za_p, pass_seq_length_p, pass_mapq_p, qualified_p, total_reads, mapq_greater_than_zero, pass_za, pass_seq_length, pass_mapq, qualified_aligned_reads) %>%
+select(mapq_gtz_p, pass_za_p, pass_seq_length_p, pass_mapq_p, qualified_p, total_reads, mapq_greater_than_zero, pass_za, pass_seq_length, pass_mapq, qualified_aligned_reads) %>%
+#select(file_name, mapq_gtz_p, pass_za_p, pass_seq_length_p, pass_mapq_p, qualified_p, total_reads, mapq_greater_than_zero, pass_za, pass_seq_length, pass_mapq, qualified_aligned_reads) %>%
 distinct() %>%
 glimpse()
 }
