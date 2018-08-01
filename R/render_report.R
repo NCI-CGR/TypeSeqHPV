3e2df9d3108c990362c8347a38f01bb14ccf818d
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-01 13:01:49 -0400

    Update render_report.R

--- R/render_report.R
+++ R/render_report.R
@@ -1,14 +1,15 @@
 #'
-render_report <- function(
-                              startPluginDf, 
+render_report <- function(startPluginDf, 
                               sample_summary_out, 
                               samples_and_controles_df_out, 
                               lineage_plot1_out, 
                               lineage_plot2_out,
-                              coinfection_rate_histogram
+                              coinfection_rate_histogram,
+                              split_deliverables
 ){
 
-system('Rscript -e \'require(devtools); install_github("cgrlab/TypeSeqHPV", force=TRUE); require(TypeSeqHPV); require(rmarkdown); run_type = "not_ts"; system(paste0("cp ", system.file("extdata/ion_report", "Ion_Torrent_report.R", package = "TypeSeqHPV")," ./")); render("Ion_Torrent_report.R")\'')
-
+render(input=system.file("extdata/ion_report", "Ion_Torrent_report.R", 
+                         package = "TypeSeqHPV"), 
+       output_dir="./", output_file="Ion_Torrent_report.pdf")
 
 }
