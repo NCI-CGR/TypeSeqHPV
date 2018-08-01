7fd82a0f83692cc3acca78b5dfda03c1bced0a46
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-01 13:10:05 -0400

    Update Ion_Torrent_report.R

--- inst/extdata/ion_report/Ion_Torrent_report.R
+++ inst/extdata/ion_report/Ion_Torrent_report.R
@@ -13,39 +13,33 @@
 
 #+  get run metadata, results='asis', echo=FALSE
 
-get_run_metadata_safe <- possibly(TypeSeqHPV::get_run_metadata, otherwise=data.frame())
-
-startPluginDf = get_run_metadata_safe(args_start_plugin_path)
+startPluginDf
 
 #' \newpage
 #' ## SAMPLE Results Summary
 
 #+ SAMPLE Results Summary, echo=FALSE, message=FALSE, warning=FALSE, fig.align = "center", results='asis', eval=TRUE
-sample_summary_safe <- possibly(TypeSeqHPV::sample_summary, otherwise=data.frame())
 
-temp = sample_summary_safe(split_deliverables$samples_only_matrix)
+sample_summary_out
 
 #' ## CONTROL Results Summary
 
 #+ CONTROL Results Summary, echo=FALSE, message=FALSE, warning=FALSE, fig.align = "center", results='asis', eval=TRUE
-control_summary_safe <- possibly(TypeSeqHPV::control_summary, otherwise=data.frame())
 
-temp = control_summary_safe(control_results$control_results_final)
+control_summary_out
+
 #' \newpage
 #' ## Counts and Percentage of Types Positive by Project
 
 #+ Counts and Percent Types Positive by Project, echo=FALSE, message=FALSE, warning=FALSE, out.width = '200%', fig.align = "center"
-percent_positive_histogram_safe <- possibly(TypeSeqHPV::percent_positve_histogram, otherwise=data.frame())
 
-temp = percent_positive_histogram_safe(split_deliverables$samples_only_matrix) 
+percent_positive_histogram_out
 
 #' \newpage
 #' ## Coinfection Rate Histogram
 #+ coinfection rate histogram, echo=FALSE, message=FALSE, warning=FALSE, out.width = '200%', fig.align = "center"
 
-coinfection_rate_histogram_safe <- possibly(TypeSeqHPV::coinfection_rate_histogram, otherwise=data.frame())
-
-temp = coinfection_rate_histogram_safe(split_deliverables$samples_only_matrix)
+coinfection_rate_histogram_out
 
 #' \newpage
 #' ## Signal-to-Noise Plot
@@ -61,29 +55,24 @@ temp = signal_to_noise_plot_safe(hpv_types_df %>% gather(HPV_Type, HPV_Type_coun
 
 #+ HPV Status Circle Plot, echo=FALSE, message=FALSE, warning=FALSE, out.width = '200%', fig.align = "center"
 
-hpv_status_circle_plot_safe <- possibly(TypeSeqHPV::hpv_status_circle_plot, otherwise=data.frame())
-
-temp = hpv_status_circle_plot_safe(split_deliverables$samples_only_matrix)
-
+signal_to_noise_plot_out
 
 #' \newpage
 #' ## HPV Positivity by Assay Plate Code
 
 #+  hpv positivity table, echo=FALSE, message=FALSE, warning=FALSE, fig.align = "center", results='asis', eval=TRUE
-samples_and_controls_df = hpv_positivity_table(split_deilverables)
 
+samples_and_controls_df_out
 
 #' \newpage
 #' ## Lineage Plots
 
 #+ lineage table plot 1, echo=FALSE, message=FALSE, warning=FALSE, fig.width=16, fig.height=9, fig.align = "center"
 
-lineage_plot_safe <- possibly(TypeSeqHPV::lineage_plot, otherwise=data.frame())
-
-temp = lineage_plot_safe(lineage_df, 1)
+lineage_plot1_out
 
 #' \newpage
 #+ normalized lineage table plot, echo=FALSE, message=FALSE, warning=FALSE, fig.width=16, fig.height=9, fig.align = "center"
-temp = lineage_plot_safe(lineage_df, 2)
 
+lineage_plot2_out
 
