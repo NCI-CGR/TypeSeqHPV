321b1f1833309979f5d89b91772eac7712381a73
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-02 00:58:23 -0400

    Update ts_demultiplex_bc2.R

--- R/ts_demultiplex_bc2.R
+++ R/ts_demultiplex_bc2.R
@@ -1,16 +1,21 @@
 #+
 
-ts_demultiplex_bc2 = function(bam_json_input, parameters_df_input, barcode_list, page, lineage_reference_table){
+ts_demultiplex_bc2 = function(bam_json_input, parameters_df_input, barcode_list, page, lineage_reference_table, bam_json_path){
 
 if("ZA" %in% colnames(bam_json_input$tags)){ ZA_df = data_frame(ZA = bam_json_input$tags$ZA)}else{ZA_df = data_frame(ZA = rep(0, length(bam_json_input$qname)))}
 
+temp = data_frame(path = bam_json_path) %>%
+separate(path, remove=FALSE, sep="IonXpress_", into=c("temp", "bc1_id")) %>%
+mutate(bc1_id = paste0("A", str_sub(bc1_id, start=2, end=3))) %>%
+select(-temp) 
+  
 bam_json = bam_json_input %>%
 select(qname, HPV_Type = rname, seq, mapq, cigar) %>%
 mutate(page_num = page) %>%
 bind_cols(ZA_df) %>%
 mutate(ZA = ifelse(is.na(ZA), 0, ZA)) %>%
-mutate(bc1_id = args_A_barcode) %>%
-#mutate(file_name = bam_json_input$name) %>%
+mutate(bc1_id = temp$bc1_id) %>%
+mutate(file_name = temp$path) %>%
 mutate(total_reads = n()) %>%
 mutate(seq_length = str_length(seq)) %>%
 left_join(parameters_df) %>%
