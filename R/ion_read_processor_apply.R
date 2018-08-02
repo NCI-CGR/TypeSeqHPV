364f4eb26a425316835df54038c16fc0deeebae7
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-01 22:46:36 -0400

    Create ion_read_processor_apply.R

---
+++ R/ion_read_processor_apply.R
@@ -0,0 +1,19 @@
+#+
+
+ion_read_processor_apply <- function(bam_json){
+mutate(path = paste0(path, ".json")) %>%
+do({
+temp = as_tibble(.)  
+temp
+mclapply(temp$path, ion_read_processor, 
+         args_lineage_reference_path=args_lineage_reference_path,
+         args_barcode_list=args_barcode_list, 
+         parameters_df=parameters_df, mc.cores=8) 
+  
+system("cat *read_metrics.json > read_metrics_merged.json")  
+system("cat *hpv_lineage.json > hpv_lineage_merged.json")  
+system("cat *bc2_demultiplex.json > bc2_demultiplex_merged.json")  
+
+temp = temp
+})
+}
