c614018527f78b922095dbc97f49356fe819fbb9
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-02 10:19:38 -0400

    Update render_report.R

--- R/render_report.R
+++ R/render_report.R
@@ -1,10 +1,10 @@
 #'
-render_report <- function(...){
+render_ion_qc_report <- function(...){
 
 require(knitr);
 require(rmarkdown)  
   
-render(input=system.file("extdata/ion_report", "Ion_Torrent_report.R", 
+render(input=system.file("reports", "Ion_Torrent_report.R", 
                          package = "TypeSeqHPV"), 
        output_dir="./", output_file="Ion_Torrent_report.pdf")
 
