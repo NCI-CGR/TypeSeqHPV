f88f6f0ffcaafa3d0201784fb37e61a3b2d64c93
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-01 23:15:24 -0400

    Update create_bam_json.R

--- R/create_bam_json.R
+++ R/create_bam_json.R
@@ -1,9 +1,9 @@
 #+
 
-create_bam_json <- function(bam_file_input) {
+create_bam_json <- function(bam_file_input, bam_dir) {
   
-bam_json_func <- function(path){
-  system(paste0("./sambamba view --nthreads=8 --format=json ", path, " -o ", path,".json"))
+bam_json_func <- function(path, ...){
+  system(paste0("./sambamba view --nthreads=8 --format=json ", bam_dir, "/", path, " -o ", path,".json"))
 }  
 
 temp = bam_file_input %>%
@@ -11,7 +11,7 @@ mutate(name = path) %>%
 do({
   temp = as_tibble(.)
   
-  parallel::mclapply(temp$path, bam_json_func, mc.cores=8)
+  parallel::mclapply(temp$path, bam_json_func, bam_dir = bam_dir, mc.cores=8)
 
   temp = temp
 
