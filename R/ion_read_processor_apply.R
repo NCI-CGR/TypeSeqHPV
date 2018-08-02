71d1b9d5c887c138f7cd41a3dd576671c3da5914
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-02 00:24:46 -0400

    Update ion_read_processor_apply.R

--- R/ion_read_processor_apply.R
+++ R/ion_read_processor_apply.R
@@ -1,13 +1,15 @@
 #+
 
-ion_read_processor_apply <- function(bam_json){
+ion_read_processor_apply <- function(bam_json, args_df, parameters_df){
+
+temp = bam_json %>%
 mutate(path = paste0(path, ".json")) %>%
 do({
 temp = as_tibble(.)  
-temp
+
 mclapply(temp$path, ion_read_processor, 
-         args_lineage_reference_path=args_lineage_reference_path,
-         args_barcode_list=args_barcode_list, 
+         args_lineage_reference_path=args_df$lineage_reference,
+         args_barcode_list=args_df$barcode_list, 
          parameters_df=parameters_df, mc.cores=8) 
   
 system("cat *read_metrics.json > read_metrics_merged.json")  
