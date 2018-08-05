12ae5174284fd841f8d0e474cf32ef014fd6f395
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-05 16:47:54 -0400

    Update workflow.R

--- workflow.R
+++ workflow.R
@@ -18,10 +18,10 @@ control_defs =opt_get('control_defs'),
 run_manifest =opt_get('run_manifest'),
 pos_neg_filtering_criteria =opt_get('pos_neg_filtering_criteria'),
 scaling_table =opt_get('scaling_table'),
-parameter_file_path =opt_get('parameter_file_path'),
+parameter_file =opt_get('parameter_file_path'),
 is_torrent_server =opt_get('is_torrent_server'),
-start_plugin_path =opt_get('start_plugin_path'),
-custom_groups_path =opt_get('custom_groups_path'))
+start_plugin =opt_get('start_plugin_path'),
+custom_groups =opt_get('custom_groups_path'))
 
 args_df %>%
 glimpse()
