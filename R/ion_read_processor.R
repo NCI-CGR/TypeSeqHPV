6225baf9f54afc6032b38f8eca6202f182a88e94
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-05 19:53:43 -0400

    Update ion_read_processor.R

--- R/ion_read_processor.R
+++ R/ion_read_processor.R
@@ -5,22 +5,13 @@ ion_read_processor <- function(bam_json_path, args_lineage_reference_path, args_
 
  print(bam_json_path)
  
- parameters_df %>%
- glimpse()
-  
-  
 lineage_reference_table = read_csv(args_lineage_reference_path) %>%
 map_if(is.factor, as.character) %>% 
-as_tibble() %>% 
-glimpse()
+as_tibble() 
   
-barcode_list = read_csv(args_barcode_list) %>%
-glimpse()
-  
-page = 1
+barcode_list = read_csv(args_barcode_list)
  
- print(paste0(bam_json_path,"_read_metrics.json"))
- print(paste0(bam_json_path[1],"_read_metricstest.json"))
+page = 1
   
 read_metrics_output = file(paste0(bam_json_path,"_read_metrics.json"), open = "wb")
 bc2_demultiplex_output = file(paste0(bam_json_path,"_bc2_demultiplex.json"), open = "wb")
@@ -28,7 +19,7 @@ lineage_output = file(paste0(bam_json_path,"_hpv_lineage.json"),open = "wb")
   
 stream_in(file(bam_json_path[1]), handler = function(df, page, bam_json_path, parameters_df, barcode_list, lineage_reference_table){
 
-print(df) 
+print(df[1,]) 
  
 read_metrics_df = df %>% 
 ts_read_metrics(parameters_df, page, bam_json_path) %>%
