5babe9a1d819a5926f7b35ffabbcb2ab4bd0fc53
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-05 18:41:36 -0400

    Update ion_read_processor_apply.R

--- R/ion_read_processor_apply.R
+++ R/ion_read_processor_apply.R
@@ -12,7 +12,7 @@ temp = as_tibble(.)
 mclapply(temp$path, ion_read_processor, 
          args_lineage_reference_path=args_lineage_reference,
          args_barcode_list=args_barcode_list, 
-         parameters_df=parameters_df, mc.cores=detectCores()) 
+         parameters_df=parameters_df, mc.cores=detectCores()-1) 
   
 system("cat *read_metrics.json > read_metrics_merged.json")  
 system("cat *hpv_lineage.json > hpv_lineage_merged.json")  
