d3411bf67e864bc59bc01d29a79dc8e9fb086825
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-05 22:02:00 -0400

    Update ts_read_metrics.R

--- R/ts_read_metrics.R
+++ R/ts_read_metrics.R
@@ -11,15 +11,17 @@ if("ZA" %in% colnames(bam_json_input$tags)){ ZA_df = data_frame(ZA = bam_json_in
 print("past ZA")
   
 temp = data_frame(path = bam_json_path) %>%
-glimpse()  %>%
-separate(path, remove=FALSE, sep="IonXpress_", into=c("temp", "bc1_id")) %>%
+tidyr::separate(path, remove=FALSE, sep="IonXpress_", into=c("temp", "bc1_id")) 
+  
+print(temp)  
+
+temp = temp %>%
 mutate(bc1_id = paste0("A", str_sub(bc1_id, start=2, end=3))) %>%
 select(-temp) %>%
 glimpse()
   
-print("temp")
+print(temp)
  
-  
 bam_json = bam_json_input %>%
 select(qname, HPV_Type = rname, seq, mapq, cigar) %>%
 mutate(page_num = page) %>%
