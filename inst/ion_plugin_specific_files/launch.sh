2a3e5f4b6c5a52749d71c49c0236f13b353beaf0
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-06 12:11:30 -0400

    Update launch.sh

--- inst/ion_plugin_specific_files/launch.sh
+++ inst/ion_plugin_specific_files/launch.sh
@@ -6,10 +6,9 @@ VERSION="1.18.08.0601"
 echo Pipeline version $VERSION
 
 ln ../../*.bam ./
-cp  ${DIRNAME}/*.csv ./
-cp  ${DIRNAME}/*.txt ./
+workDir=pwd
 
-sudo docker run -it -v /home/ubuntu/downloads:/mnt cgrlab/typeseqhpv:final_2018080301 Rscript /mnt/TypeSeqHPV/workflow.R \
+sudo docker run -it -v $workDir:/mnt cgrlab/typeseqhpv:final_2018080301 Rscript /TypeSeqHPV/workflow.R \
 --bam_header /mnt/IonXpress_087_rawlib.txt \
 --bam_files_dir /mnt/ \
 --lineage_reference /mnt/20180121_lineage_reference_table_and_filters_2.csv \
@@ -19,7 +18,7 @@ sudo docker run -it -v /home/ubuntu/downloads:/mnt cgrlab/typeseqhpv:final_20180
 --pos_neg_filtering_criteria /mnt/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
 --scaling_table /mnt/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
 --parameter_file /mnt/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
---is_torrent_server No --start_plugin /mnt/startplugin.json \
+--start_plugin /mnt/startplugin.json \
 --custom_groups /mnt/report_grouping.csv \
 --is_torrent_server "yes" | true
 
