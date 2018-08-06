7f70618bc627f6c45bd04e24742c8c0a99033ebb
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-06 12:27:13 -0400

    Update startplugin_parse.R

--- R/startplugin_parse.R
+++ R/startplugin_parse.R
@@ -9,16 +9,21 @@ manifest = data_frame(values = plugin_json$pluginconfig$typing_manifest) %>%
 mutate(values = str_replace(values, "\n", "" )) %>%
 separate(col=values, sep=",", into = unlist(str_split(.$values[1], ","))) %>%
 slice(2:n()) %>%
+glimpse() %>%
 write_csv("typing_manifest.csv")
-  
   
 report_grouping = data_frame(values = plugin_json$pluginconfig$report_grouping_definitions) %>%
 mutate(values = str_replace(values, "\n", "" )) %>%
 separate(col=values, sep=",", into = unlist(str_split(.$values[1], ","))) %>%
 slice(2:n()) %>%
+glimpse() %>%
 write_csv("report_grouping.csv")
   
+control_defs = data_frame(values = plugin_json$pluginconfig$control_definitions) %>%
+mutate(values = str_replace(values, "\n", "" )) %>%
+separate(col=values, sep=",", into = unlist(str_split(.$values[1], ","))) %>%
+slice(2:n()) %>%
+glimpse() %>%
+write_csv("control_defs.csv")
   
-  
-
 }
