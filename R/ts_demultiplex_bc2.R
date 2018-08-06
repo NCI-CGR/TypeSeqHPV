2a2ebc111e316f087329c8ba200b96829f79022d
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-06 11:02:51 -0400

    Update ts_demultiplex_bc2.R

--- R/ts_demultiplex_bc2.R
+++ R/ts_demultiplex_bc2.R
@@ -2,19 +2,13 @@
 
 ts_demultiplex_bc2 = function(bam_json_input, parameters_df, barcode_list, page, lineage_reference_table, bam_json_path){
 
-   
- print(bam_json_input)  
-   
-   
 #library(GenomicAlignments)
 
 if("ZA" %in% colnames(bam_json_input$tags)){ ZA_df = data_frame(ZA = bam_json_input$tags$ZA)}else{ZA_df = data_frame(ZA = rep(0, length(bam_json_input$qname)))}
 
 temp = data_frame(path = bam_json_path)
    
- print(temp)  
-   
-temp = temp   %>%
+ temp = temp   %>%
 tidyr::separate(path, remove=FALSE, sep="IonXpress_", into=c("temp", "bc1_id")) %>%
 mutate(bc1_id = paste0("A", str_sub(bc1_id, start=2, end=3))) %>%
 select(-temp) 
