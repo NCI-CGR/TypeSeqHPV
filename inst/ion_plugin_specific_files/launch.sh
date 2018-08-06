5fe17159265d77f9297788e627d4b70c8fffa3f8
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-06 13:07:28 -0400

    Update launch.sh

--- inst/ion_plugin_specific_files/launch.sh
+++ inst/ion_plugin_specific_files/launch.sh
@@ -9,17 +9,17 @@ ln ../../*.bam ./
 workDir=pwd
 
 sudo docker run -it -v $workDir:/mnt cgrlab/typeseqhpv:final_2018080301 Rscript /TypeSeqHPV/workflow.R \
---bam_header /mnt/IonXpress_087_rawlib.txt \
+--pos_neg_filtering_criteria /TypeSeqHPV/docs/Ion/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
+--scaling_table /TypeSeqHPV/docs/Ion/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
+--parameter_file /TypeSeqHPV/docs/Ion/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
+--lineage_reference /TypeSeqHPV/docs/Ion/20180121_lineage_reference_table_and_filters_2.csv \
+--barcode_list /TypeSeqHPV/docs/Ion/barcodeList_v1.csv \
 --bam_files_dir /mnt/ \
---lineage_reference /mnt/20180121_lineage_reference_table_and_filters_2.csv \
---barcode_list /mnt/barcodeList_v1.csv \
---control_defs /mnt/control_defs.csv \
---run_manifest /mnt/typing_manifest.csv \
---pos_neg_filtering_criteria /mnt/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
---scaling_table /mnt/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
---parameter_file /mnt/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
 --start_plugin /mnt/startplugin.json \
 --custom_groups /mnt/report_grouping.csv \
+--control_defs /mnt/control_defs.csv \
+--run_manifest /mnt/typing_manifest.csv \
+--bam_header /TypeSeqHPV/docs/Ion/IonXpress_087_rawlib.txt \
 --is_torrent_server "yes" | true
 
 rm *.bam | true
