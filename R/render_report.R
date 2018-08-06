4c7829943b84232379d5b639b7673e57f4eae28b
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-06 04:25:56 -0400

    Update render_report.R

--- R/render_report.R
+++ R/render_report.R
@@ -4,6 +4,8 @@ render_ion_qc_report <- function(...){
 require(knitr)
 require(rmarkdown)  
 require(TypeSeqHPV)
+require(scales)
+require(ggsci)
   
 render(input=system.file("reports", "Ion_Torrent_report.R", 
                          package = "TypeSeqHPV"), 
