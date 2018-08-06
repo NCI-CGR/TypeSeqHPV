933493ca9a707d673ddf2880c1fc56e1d742e562
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-05 22:58:09 -0400

    Update ts_read_metrics.R

--- R/ts_read_metrics.R
+++ R/ts_read_metrics.R
@@ -4,12 +4,8 @@ ts_read_metrics <- function(bam_json_input, parameters_df, page, bam_json_path){
 
 require(TypeSeqHPV)
 
-# only used in a jsonlite streaming function test
-
 if("ZA" %in% colnames(bam_json_input$tags)){ ZA_df = data_frame(ZA = bam_json_input$tags$ZA)}else{ZA_df = data_frame(ZA = rep(0, length(bam_json_input$qname)))}
 
-print("past ZA")
-  
 temp = data_frame(path = bam_json_path) %>%
 tidyr::separate(path, remove=FALSE, sep="IonXpress_", into=c("temp", "bc1_id")) 
   
