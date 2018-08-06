ad0534f053daf47e6fd6164debe86b7901a76387
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-06 11:51:44 -0400

    Update launch.sh

--- inst/ion_plugin_specific_files/launch.sh
+++ inst/ion_plugin_specific_files/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 # TypeSeq HPV Plugin
-VERSION="1.18.07.2601"
+VERSION="1.18.08.0601"
 #autorundisable
 echo Pipeline version $VERSION
 
@@ -9,20 +9,19 @@ ln ../../*.bam ./
 cp  ${DIRNAME}/*.csv ./
 cp  ${DIRNAME}/*.txt ./
 
-FILES=`ls *.bam`
-
-/results/plugins/scratch/rabix/rabix -b ./  ${DIRNAME}/TypeSeqHPV_ion_torrent_workflow.cwl -- \
---input `echo $FILES | sed 's/ / --input /g'` \
---mem_mb 8000 \
---parameter_file hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
---pos_neg_filtering_criteria_path 2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
---run_manifest startplugin.json \
---control_definitions startplugin.json \
---report_grouping startplugin.json \
---scaling_table 2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
---barcode_list barcodeList_v1.txt \
---lineage_reference_table 20180121_lineage_reference_table_and_filters_2.csv \
---is_torrent_server | true
+sudo docker run -it -v /home/ubuntu/downloads:/mnt cgrlab/typeseqhpv:final_2018080301 Rscript /mnt/TypeSeqHPV/workflow.R \
+--bam_header /mnt/IonXpress_087_rawlib.txt \
+--bam_files_dir /mnt/ \
+--lineage_reference /mnt/20180121_lineage_reference_table_and_filters_2.csv \
+--barcode_list /mnt/barcodeList_v1.csv \
+--control_defs /mnt/control_defs.csv \
+--run_manifest /mnt/typing_manifest.csv \
+--pos_neg_filtering_criteria /mnt/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
+--scaling_table /mnt/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
+--parameter_file /mnt/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
+--is_torrent_server No --start_plugin /mnt/startplugin.json \
+--custom_groups /mnt/report_grouping.csv \
+--is_torrent_server "yes" | true
 
 rm *.bam | true
 rm */root/*/*/*filtered.json | true
