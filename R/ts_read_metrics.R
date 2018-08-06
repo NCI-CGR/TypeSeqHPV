4d407724f8b78940c13260b274f9dd7824246f1b
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-05 20:05:25 -0400

    Update ts_read_metrics.R

--- R/ts_read_metrics.R
+++ R/ts_read_metrics.R
@@ -9,6 +9,7 @@ require(TypeSeqHPV)
 if("ZA" %in% colnames(bam_json_input$tags)){ ZA_df = data_frame(ZA = bam_json_input$tags$ZA)}else{ZA_df = data_frame(ZA = rep(0, length(bam_json_input$qname)))}
 
 temp = data_frame(path = bam_json_path) %>%
+glimpse() %>%
 separate(path, remove=FALSE, sep="IonXpress_", into=c("temp", "bc1_id")) %>%
 mutate(bc1_id = paste0("A", str_sub(bc1_id, start=2, end=3))) %>%
 select(-temp) %>%
