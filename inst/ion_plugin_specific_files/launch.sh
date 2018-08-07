56387cb46edbd06a109e00dfccc6581ed97a908b
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-07 15:09:55 -0400

    Update launch.sh

--- inst/ion_plugin_specific_files/launch.sh
+++ inst/ion_plugin_specific_files/launch.sh
@@ -7,7 +7,7 @@ echo Pipeline version $VERSION
 
 ln ../../*.bam ./
 
-docker run -v $(pwd):/mnt cgrlab/typeseqhpv:final_2018080601 Rscript /mnt/TypeSeqHPV/inst/workflows/ion_workflow.R \
+docker run -v $(pwd):/mnt cgrlab/typeseqhpv:final_2018080601 Rscript /TypeSeqHPV/workflows/ion_workflow.R \
 --pos_neg_filtering_criteria /TypeSeqHPV/docs/Ion/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
 --scaling_table /TypeSeqHPV/docs/Ion/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
 --parameter_file /TypeSeqHPV/docs/Ion/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
