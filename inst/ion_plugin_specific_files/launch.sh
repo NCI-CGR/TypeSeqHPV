b27e769671e6fddba30fc209c4f9a9bfa8e83a11
davidroberson <david.roberson@gmail.com>
2018-08-08 14:53:22 +0000

    adding -i

--- inst/ion_plugin_specific_files/launch.sh
+++ inst/ion_plugin_specific_files/launch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
 # TypeSeq HPV Plugin
-VERSION="1.18.08.0801"
+VERSION="1.18.08.0802"
 #autorundisable
 echo Pipeline version $VERSION
 
 ln ../../*.bam ./
 
-docker run -v $(pwd):/mnt cgrlab/typeseqhpv:final_2018080701 Rscript /TypeSeqHPV/workflows/ion_workflow.R \
+docker run -i -v $(pwd):/mnt cgrlab/typeseqhpv:final_2018080701 Rscript /TypeSeqHPV/workflows/ion_workflow.R \
 --pos_neg_filtering_criteria /TypeSeqHPV/docs/Ion/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
 --scaling_table /TypeSeqHPV/docs/Ion/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
 --parameter_file /TypeSeqHPV/docs/Ion/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
@@ -19,7 +19,7 @@ docker run -v $(pwd):/mnt cgrlab/typeseqhpv:final_2018080701 Rscript /TypeSeqHPV
 --control_defs /mnt/control_defs.csv \
 --run_manifest /mnt/typing_manifest.csv \
 --bam_header /TypeSeqHPV/docs/Ion/IonXpress_087_rawlib.txt \
---is_torrent_server "yes" | true
+--is_torrent_server "yes"
 
 # check to see if this is still required
 # rm *.bam | true 
