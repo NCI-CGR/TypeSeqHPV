ad7f4fe59f942e598bfbda0ea5a31b4723f6330a
davidroberson <david.roberson@gmail.com>
2018-08-08 01:31:43 +0000

    added plugin zip

--- inst/ion_plugin_specific_files/create_plugin_zip.sh
+++ inst/ion_plugin_specific_files/create_plugin_zip.sh
@@ -1,20 +1,11 @@
-# pull latest github repo
-rm -r TypeSeqHPV
-mkdir TypeSeqHPV
-cd TypeSeqHPV
-git init
-git pull https://github.com/cgrlab/TypeSeqHPV.git
-cd ../
 rm -r TypeSeqHPV-TSv1-DEV
 mkdir TypeSeqHPV-TSv1-DEV
-cd TypeSeqHPV-TSv1-DEV
 
 # ion torrent plugin specific files
-cp ../TypeSeqHPV/inst/extdata/ion_plugin_specific_files/instance.html ./
-cp ../TypeSeqHPV/inst/extdata/ion_plugin_specific_files/launch.sh ./
-cp ../TypeSeqHPV/inst/extdata/ion_plugin_specific_files/plan.html ./
-cp ../TypeSeqHPV/inst/extdata/ion_plugin_specific_files/pluginsettings.json ./
+cp inst/ion_plugin_specific_files/instance.html TypeSeqHPV-TSv1-DEV
+cp inst/ion_plugin_specific_files/launch.sh TypeSeqHPV-TSv1-DEV
+cp inst/ion_plugin_specific_files/plan.html TypeSeqHPV-TSv1-DEV
+cp inst/ion_plugin_specific_files/pluginsettings.json TypeSeqHPV-TSv1-DEV
 
 # zip plugin package
-cd ../
 zip -r TypeSeqHPV_TSv1_Ion_Torrent_Plugin_Dev.zip TypeSeqHPV-TSv1-DEV
--- inst/ion_plugin_specific_files/launch.sh
+++ inst/ion_plugin_specific_files/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 # TypeSeq HPV Plugin
-VERSION="1.18.08.0601"
+VERSION="1.18.08.0701"
 #autorundisable
 echo Pipeline version $VERSION
 
