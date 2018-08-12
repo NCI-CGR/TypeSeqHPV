e4d53b09bdcec537cc143fe2cd0ad03d574b1641
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-12 19:41:35 -0400

    Update ion_read_processor.R

--- R/ion_read_processor.R
+++ R/ion_read_processor.R
@@ -13,19 +13,19 @@ barcode_list = read_csv(args_barcode_list)
  
 page = 1
   
-read_metrics_output = file(paste0(bam_json_path,"_read_metrics.json"), open = "wb")
-bc2_demultiplex_output = file(paste0(bam_json_path,"_bc2_demultiplex.json"), open = "wb")
-lineage_output = file(paste0(bam_json_path,"_hpv_lineage.json"),open = "wb")
+read_metrics_output = file(paste0(bam_json_path$path,"_read_metrics.json"), open = "wb")
+bc2_demultiplex_output = file(paste0(bam_json_path$path,"_bc2_demultiplex.json"), open = "wb")
+lineage_output = file(paste0(bam_json_path$path,"_hpv_lineage.json"),open = "wb")
   
-stream_in(file(bam_json_path), handler = function(df){
+stream_in(file(bam_json_path$path), handler = function(df){
  
 read_metrics_df = df %>% 
-ts_read_metrics(parameters_df, page, bam_json_path)
+ts_read_metrics(parameters_df, page, bam_json_path$path)
  
-print(bam_json_path)
+print(bam_json_path$path)
 
 bc2_demultiplex_df = df %>% 
-ts_demultiplex_bc2(parameters_df, barcode_list, page, lineage_reference_table, bam_json_path)
+ts_demultiplex_bc2(parameters_df, barcode_list, page, lineage_reference_table, bam_json_path$path)
 
 stream_out(read_metrics_df, read_metrics_output, verbose = TRUE) 
   
@@ -41,6 +41,6 @@ close(read_metrics_output)
 close(bc2_demultiplex_output)
 close(lineage_output)
  
-return(bam_json_path)  
+return(bam_json_path$path)  
   
 }
