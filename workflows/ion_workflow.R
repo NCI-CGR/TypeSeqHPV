288012d6b212fa2ad046047be0299a14df9cb0cb
davidroberson <david.roberson@gmail.com>
2018-08-13 20:50:54 +0000

    furrr replaces mclapply

--- workflows/ion_workflow.R
+++ workflows/ion_workflow.R
@@ -1,3 +1,4 @@
+################################# load packages #################################
 require(devtools)
 #install_github("cgrlab/TypeSeqHPV", force=TRUE)
 require(TypeSeqHPV)
@@ -8,9 +9,10 @@ library(igraph)
 library(jsonlite)
 library(parallel)
 library(rmarkdown)
+library(furrr)
+library(future)
 
-setwd("/mnt")
-
+################################# get comman line arguments #################################
 args_bam_files_dir = opt_get('bam_files_dir')
 args_lineage_reference = opt_get('lineage_reference')
 args_bam_header = opt_get('bam_header')
@@ -24,70 +26,83 @@ args_is_torrent_server =opt_get('is_torrent_server')
 args_start_plugin =opt_get('start_plugin')
 args_custom_groups =opt_get('custom_groups')
 
+################################# analysis plan #################################
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
-
-parse_startplugin_plan <- drake_plan(
-  parse = startplugin_parse(args_start_plugin))
-
-
 ion_plan <- drake_plan(
-################################# bam input #################################
-bam_file_input = data_frame(path = dir(args_bam_files_dir, pattern=".bam", full.names = FALSE)),
+parse = startplugin_parse(args_start_plugin),
+  
+###
+bam_json = data_frame(path = dir("/mnt", pattern=".bam", full.names = FALSE))  %>%
+filter(!(str_detect(path, "json"))) %>%
+split(.$path) %>%
+future_map_dfr(convert_bam_to_json, args_bam_files_dir),
 
-################################# bam_json #################################
-bam_json = create_bam_json(bam_file_input, args_bam_files_dir), 
-  
-################################# ion_read_processing #################################
-ion_read_processing_df = ion_read_processor_apply(bam_json, args_lineage_reference, args_barcode_list, parameters_df),
-  
-################################# parameters file input #################################
-parameters_csv_input = args_parameter_file,
+###
+ion_read_processing_df = bam_json %>% 
+mutate(path = paste0(path, ".json")) %>%
+glimpse() %>%
+split(.$path) %>%
+future_map_dfr(ion_read_processor,
+              args_lineage_reference_path=args_lineage_reference,
+              args_barcode_list=args_barcode_list,
+              parameters_df=parameters_df) %>%
+do({
+system("cat *read_metrics.json > read_metrics_merged.json")
+system("cat *hpv_lineage.json > hpv_lineage_merged.json")
+system("cat *bc2_demultiplex.json > bc2_demultiplex_merged.json") 
+temp = as_tibble(.)}),
 
-################################# parameters dataframe #################################
-parameters_df = TypeSeqHPV::read_in_parameters_csv(parameters_csv_input),
-  
-################################# bam header df  #################################
+###
+parameters_df = TypeSeqHPV::read_in_parameters_csv(file_in(args_parameter_file)),
+
+###  
 bam_header_df = TypeSeqHPV::read_in_bam_header(file_in(args_bam_header)),
 
-################################# hpv types dataframe #################################
+###
 hpv_types_df = TypeSeqHPV::create_hpv_types_table(ion_read_processing_df, args_run_manifest, bam_header_df, parameters_df),
 
-################################# read metrics df #################################
+###
 read_metrics_df = TypeSeqHPV::create_full_run_read_metrics_df(ion_read_processing_df, 
                                                   hpv_type_counts=hpv_types_df),
 
-################################# scaling of b2m control #################################
+###
 scaling_list = TypeSeqHPV::scaling_of_b2m_human_control(read_metrics_df, 
                                                   args_run_manifest, 
                                                   args_scaling_table, 
                                                   args_pos_neg_filtering_criteria) %>% glimpse(),
 
-#################################initial pn matrix #################################
+###
 initial_pn_matrix = TypeSeqHPV::create_inital_pos_neg_matrix(hpv_types_df, 
                                                                scaling_list$factoring_table, scaling_list$filtering_criteria),
 
 
-################################# expected control results #################################
+
+###
 control_results = TypeSeqHPV::calculate_expected_control_results(args_control_defs, initial_pn_matrix),
- 
-################################# finalize pn matrix #################################
+
+
+### 
 final_pn_matrix = TypeSeqHPV::finalize_pn_matrix(initial_pn_matrix, 
                                                  scaling_list$filtering_criteria, 
                                                  control_results$control_results_final),
 
-################################# split pn matrix outputs #################################
+
+###
 split_deliverables = TypeSeqHPV::split_pn_matrix_into_multiple_deliverables(final_pn_matrix, 
                                                                             control_results$control_results_final),
 
-################################# prep grouped samples #################################
+
+###
 grouped_samples_only_matrix = TypeSeqHPV::prepare_grouped_samples_only_matrix_outputs(args_custom_groups, 
                                                                                       split_deliverables$samples_only_matrix,
                                                                                       parameters_df),
-################################# lineage df #################################
+
+###
 lineage_df = TypeSeqHPV::prepare_lineage_df(args_lineage_reference, 
                                               ion_read_processing, split_deliverables$samples_only_matrix),
 
-################################# create csv files #################################
+
+###
 collection_of_csv_files = TypeSeqHPV::write_all_csv_files(final_grouped_samples_only_matrix=grouped_samples_only_matrix, 
                      read_metrics=read_metrics_df,
                      final_pn_matrix = final_pn_matrix, 
@@ -97,7 +112,8 @@ collection_of_csv_files = TypeSeqHPV::write_all_csv_files(final_grouped_samples_
                      full_lineage_table_with_manifest=lineage_df,
                      parameters_df = parameters_df),
 
-################################# ion qc knitr report #################################
+
+###
 ion_qc_report = render_ion_qc_report(args_start_plugin=args_start_plugin, 
                               split_deliverables=split_deliverables, 
                               samples_and_controls_df_out=samples_and_controls_df_out, 
@@ -108,16 +124,15 @@ ion_qc_report = render_ion_qc_report(args_start_plugin=args_start_plugin,
                              lineage_df=lineage_df,
                              bam_header_df = bam_header_df),
 
-rename_report = system(paste0("cp Ion_Torrent_report.pdf ", final_pn_matrix$Assay_Batch_Code[1], "_qc_report.pdf")),
 
-################################# create plugin html block #################################
+###
+rename_report = system(paste0("cp Ion_Torrent_report.pdf ", final_pn_matrix$Assay_Batch_Code[1], "_qc_report.pdf"))
 
-html_block = render("/TypeSeqHPV/inst/reports/torrent_server_html_block.R", output_dir = "/mnt")
 
 )
 
-
-make(parse_startplugin_plan)
+setwd("/mnt")
+future::plan(multiprocess)
 make(ion_plan)
 
 
