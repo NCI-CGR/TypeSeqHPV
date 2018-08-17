c27891ba4b9f58a96ffa0573b4e7e78a145b599d
Dave Roberson <davidroberson@users.noreply.github.com>
2018-08-17 16:32:43 -0400

    rm /mnt

--- R/samtools_extract_header.R
+++ R/samtools_extract_header.R
@@ -2,5 +2,5 @@
 extract_header <- function(bam_dir, bam_files){
   require(dplyr)
   system(paste0("samtools view ", bam_dir, "/", bam_files$path[1], " -H -o bam_header.txt"))
-  df = data_frame(path = "/mnt/bam_header.txt")
+  df = data_frame(path = "bam_header.txt")
 }  
