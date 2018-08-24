26bebc2389b1e8f55c69639ff88e20e64aea7e39
davidroberson <david.roberson@gmail.com>
2018-08-24 16:05:06 +0000

    fixed toc

--- inst/ion_plugin_specific_files/launch.sh
+++ inst/ion_plugin_specific_files/launch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
 # TypeSeq HPV Plugin
-VERSION="1.18.08.2204"
+VERSION="1.18.08.2401"
 #autorundisable
 echo Pipeline version $VERSION
 
 ln ../../*.bam ./
 
-docker run -i -v $(pwd):/mnt cgrlab/typeseqhpv:final_18082202 Rscript /TypeSeqHPV/workflows/ion_workflow.R \
+docker run -i -v $(pwd):/mnt cgrlab/typeseqhpv:final_18082401 Rscript /TypeSeqHPV/workflows/ion_workflow.R \
 --pos_neg_filtering_criteria /TypeSeqHPV/docs/Ion/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
 --scaling_table /TypeSeqHPV/docs/Ion/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
 --parameter_file /TypeSeqHPV/docs/Ion/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
@@ -21,7 +21,7 @@ docker run -i -v $(pwd):/mnt cgrlab/typeseqhpv:final_18082202 Rscript /TypeSeqHP
 --is_torrent_server "yes"
 
 #rm extra bam and bam.json files
-rm *.bam* | true
+rm *.bam*
 
 
 
