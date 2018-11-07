5ed70660a54776112704fa8bc6e039627b590575
davidroberson <david.roberson@gmail.com>
2018-11-07 18:16:48 +0000

    added spark to base_Dockerfile

--- inst/methylation/launch.sh
+++ inst/methylation/launch.sh
@@ -7,12 +7,22 @@ echo Pipeline version $VERSION
 
 ln ../../*.bam ./
 
+docker run -i -v $(pwd):/mnt cgrlab/typeseqhpv:final_18082401 Rscript /TypeSeqHPV/workflows/ion_workflow.R \
+--pos_neg_filtering_criteria /TypeSeqHPV/docs/Ion/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
+--scaling_table /TypeSeqHPV/docs/Ion/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
+--parameter_file /TypeSeqHPV/docs/Ion/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
+--lineage_reference /TypeSeqHPV/docs/Ion/20180121_lineage_reference_table_and_filters_2.csv \
+--barcode_list /TypeSeqHPV/docs/Ion/barcodeList_v1.txt \
+--bam_files_dir /mnt/ \
+--start_plugin /mnt/startplugin.json \
+--custom_groups /mnt/report_grouping.csv \
+--control_defs /mnt/control_defs.csv \
+--run_manifest /mnt/typing_manifest.csv \
+--is_torrent_server "yes"
+
 docker run -i -v $(pwd):/mnt \
     -v /results/plugins/TypeSeqHPVMethyl-DEV:/data \
     cgrlab/typeseqhpv:adam_18102901 \
     /bin/sh -c 'cd /mnt; /home/adam/bin/adam-shell -i /data/demux_3prime_barcode_adam.scala'
 
 rm *.bam
-
-
-
