f3bb3d6036604aaa8be1d42eb3cf555a7ae91814
davidroberson <david.roberson@gmail.com>
2018-11-20 19:07:09 +0000

    correcting sambamba location

--- R/convert_bam_to_json.R
+++ R/convert_bam_to_json.R
@@ -2,6 +2,6 @@
 
 convert_bam_to_json <- function(path, bam_dir){
   require(dplyr)
-  system(paste0("/sambamba view --nthreads=8 --format=json ", bam_dir, "/", path, " -o ", path,".json"))
+  system(paste0("/home/adam/sambamba view --nthreads=8 --format=json ", bam_dir, "/", path, " -o ", path,".json"))
   df = data_frame(path = unlist(path))
-}  
+}
