80418cd103884240c574aeca53fe847874d28bd7
davidroberson <david.roberson@gmail.com>
2019-02-07 15:02:37 +0000

    demux

--- R/adam_demux.R
+++ R/adam_demux.R
@@ -5,8 +5,8 @@ adam_demux <- function(...){
 
     system("/home/adam/bin/adam-shell --driver-memory 80G --driver-cores 22 -i /TypeSeqHPV/inst/methylation/demux_3prime_barcode_adam.scala")
     system("mkdir demux_bams")
-    system("mv A*bam/*.bam demux_bams")
-    #system("rm -R *_demux")
+    system("mv *.bam_demux/*.bam demux_bams")
+    system("rm -R *_demux")
 
     return(
         dir_ls("demux_bams", glob = "*.bam") %>%
