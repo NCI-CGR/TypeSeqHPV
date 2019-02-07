88ee66edd98dccff0affd0c557d4ab5da10efbfd
davidroberson <david.roberson@gmail.com>
2019-02-07 21:39:03 +0000

    vcf work

--- R/vcf_2_dataframe.R
+++ R/vcf_2_dataframe.R
@@ -3,8 +3,13 @@ vcf_to_dataframe <- function(vcf_files){
     require(dplyr)
     require(fs)
     require(VCFWrenchR)
+    require(VariantAnnotation)
+
+    detach("package:drake", unload = TRUE)
 
     temp = as.data.frame(readVcf(vcf_files$path))
+
+    require(drake)
 
     return(temp)
 
