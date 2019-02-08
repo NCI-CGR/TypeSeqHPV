596ef4d4857e1df12fee1fc1c33c171aabf33d95
davidroberson <david.roberson@gmail.com>
2019-02-08 22:19:23 +0000

    did not specify driver cores or ram

--- R/adam_demux.R
+++ R/adam_demux.R
@@ -3,7 +3,7 @@ adam_demux <- function(...){
     require(dplyr)
     require(fs)
 
-    system("/home/adam/bin/adam-shell --driver-memory 80G --driver-cores 22 -i /TypeSeqHPV/inst/methylation/demux_3prime_barcode_adam.scala")
+    system("/home/adam/bin/adam-shell -i /TypeSeqHPV/inst/methylation/demux_3prime_barcode_adam.scala")
     system("mkdir demux_bams")
     system("mv *.bam_demux/*.bam demux_bams")
     system("rm -R *_demux")
