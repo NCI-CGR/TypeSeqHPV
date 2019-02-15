6bb9ae299de826d2182cf6ed405430ebb7ad125e
davidroberson <david.roberson@gmail.com>
2019-02-15 04:07:22 +0000

    sam tools

--- R/samtools_sort.R
+++ R/samtools_sort.R
@@ -4,9 +4,9 @@ samtools_sort <- function(bam_files){
 
     bam_df = bam_files %>%
         as_tibble() %>%
-        separate(path, "demux_bams/", into = c("temp", "bam"), remove = FALSE) %>%
-        select(-temp) %>%
-        separate(bam, ".rawlib.bam", into = c("bam"), extra = 'drop') %>%
+        #separate(path, "./", into = c("temp", "bam"), remove = FALSE) %>%
+        #select(-temp) %>%
+        separate(bam, ".bam", into = c("bam"), extra = 'drop') %>%
         rename(bam_path = path) %>%
         mutate(sorted_path = paste0("sorted_bams/", bam,".sorted"))
 
