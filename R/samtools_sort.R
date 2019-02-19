6eecee7fa7b6f941962fd01f1b27ab99285c3ab7
davidroberson <david.roberson@gmail.com>
2019-02-19 03:47:14 +0000

    fixing index

--- R/samtools_sort.R
+++ R/samtools_sort.R
@@ -2,23 +2,11 @@
 samtools_sort <- function(bam_files){
     require(tidyverse)
 
-    bam_df = bam_files %>%
-        as_tibble() %>%
-        #separate(path, "./", into = c("temp", "bam"), remove = FALSE) %>%
-        #select(-temp) %>%
-        mutate(bam = path) %>%
-        separate(bam, ".bam", into = c("bam"), extra = 'drop') %>%
-        rename(bam_path = path) %>%
-        mutate(sorted_path = paste0("sorted_bams/", bam,".sorted"))
+    #system(paste0("samtools sort ", bam_df$bam_path, " ", bam_df$sorted_path), wait = TRUE)
 
-    system(paste0("samtools sort ", bam_df$bam_path, " ", bam_df$sorted_path), wait = TRUE)
+    system(paste0("samtools index ", paste0(bam_files$path, ".bam")), wait = TRUE)
 
-    system(paste0("samtools index ", paste0(bam_df$sorted_path, ".bam")), wait = TRUE)
-
-    bam_df = bam_df %>%
-        mutate(sorted_path = paste0(sorted_path, ".bam"))
-
-    return(bam_df)
+    return(bam_files)
 
 }
 
