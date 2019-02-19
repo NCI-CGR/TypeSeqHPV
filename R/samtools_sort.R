77b33fb13ba18a17df13a4c5ff8ffe242249f8de
davidroberson <david.roberson@gmail.com>
2019-02-19 04:06:47 +0000

    index

--- R/samtools_sort.R
+++ R/samtools_sort.R
@@ -2,9 +2,15 @@
 samtools_sort <- function(bam_files){
     require(tidyverse)
 
+
+    temp = bam_files %>%
+        ungroup() %>%
+        select(path) %>%
+        distinct()
+
     #system(paste0("samtools sort ", bam_df$bam_path, " ", bam_df$sorted_path), wait = TRUE)
 
-    system(paste0("samtools index ", paste0(bam_files$path, ".bam")), wait = TRUE)
+    system(paste0("samtools index ", temp$path), wait = TRUE)
 
     return(bam_files)
 
