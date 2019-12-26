b428b0e7fdc054594e9bf98c289d87e342748288
Dave Roberson <davidroberson@users.noreply.github.com>
2019-12-26 13:49:43 -0500

    updated launch.sh to include development tag

--- inst/typeseq2/launch.sh
+++ inst/typeseq2/launch.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -x
 # TypeSeq2 HPV
-VERSION="1.1903.2101"
+VERSION="2.1912.2601"
 #autorundisable
 echo Pipeline version $VERSION
 
 ln ../../*.bam ./
 
 docker run -i -v $(pwd):/mnt -v /mnt:/user_files \
-    cgrlab/typeseqhpv:dev_19032203 \
+    cgrlab/typeseqhpv:development_191226 \
         Rscript /TypeSeqHPV/workflows/TypeSeq2.R \
         --is_torrent_server yes \
         --config_file config_file.csv \
         --barcode_file barcodes.csv \
-        --control_definitions control_defs.csv \
+        --control_definitions control_defs.csv
         --cores 22 \
         --manifest manifest.csv \
         --ram 80G \
