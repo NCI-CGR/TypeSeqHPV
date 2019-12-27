ac55d6d5ee777ae276aa1979dcac539a46ba7499
Dave Roberson <davidroberson@users.noreply.github.com>
2019-12-27 09:18:13 -0500

    added

--- inst/typeseq2/launch.sh
+++ inst/typeseq2/launch.sh
@@ -13,7 +13,7 @@ docker run -i -v $(pwd):/mnt -v /mnt:/user_files \
         --is_torrent_server yes \
         --config_file config_file.csv \
         --barcode_file barcodes.csv \
-        --control_definitions control_defs.csv
+        --control_definitions control_defs.csv \
         --cores 22 \
         --manifest manifest.csv \
         --ram 80G \
