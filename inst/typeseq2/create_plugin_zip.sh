cca6db14cb785fe90c2ae5f86000e61272860757
Shastry <shastrya2@nci-02134343-ml.nci.nih.gov>
2019-12-27 13:48:30 -0500

    Changed TypeSeq2 to TypeSeq2-Dev

--- inst/typeseq2/create_plugin_zip.sh
+++ inst/typeseq2/create_plugin_zip.sh
@@ -1,11 +1,11 @@
-rm -R TypeSeq2
-mkdir TypeSeq2
+rm -R TypeSeq2-Dev
+mkdir TypeSeq2-Dev
 
 # ion torrent plugin specific files
-cp inst/typeseq2/instance.html TypeSeq2/
-cp inst/typeseq2/launch.sh TypeSeq2/
-cp inst/typseq2/plan.html TypeSeq2/
-cp inst/typeseq2/pluginsettings.json TypeSeq2/
+cp inst/typeseq2/instance.html TypeSeq2-Dev/
+cp inst/typeseq2/launch.sh TypeSeq2-Dev/
+cp inst/typeseq2/plan.html TypeSeq2-Dev/
+cp inst/typeseq2/pluginsettings.json TypeSeq2-Dev/
 
 # zip plugin package
-zip -r TypeSeq2.zip TypeSeq2
+zip -r TypeSeq2-Dev.zip TypeSeq2-Dev
