70a3eef4b5a3c6b77a4f8eb733282badb50656c0
Amulya Shastry <amulyashastry@gmail.com>
2020-02-05 10:25:39 -0500

    Update render_report.R

--- R/render_report.R
+++ R/render_report.R
@@ -2,7 +2,6 @@
 render_ion_qc_report <- function(args_start_plugin,
                                  control_for_report,
                                  samples_only_for_report,
-                                 samples_and_controls_df_out,
                                  control_results,
                                  read_count_matrix_report,
                                  detailed_pn_matrix_for_report,
