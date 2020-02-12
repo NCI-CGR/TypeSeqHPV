d11c4ee5b0ca2bf13bcb3c5d0ae20a7c686fb539
Shastry <shastrya2@nci-02134343-ml.nci.nih.gov>
2020-02-12 17:42:58 -0500

    Adding a new feature to the report

---
+++ R/Internal_control_summary.R
@@ -0,0 +1,69 @@
+#'
+#'control_summary
+
+#Using detailed_pn_matrix, manifest and control results
+
+control_summary <- function(detailed_pn_matrix,manifest,control_for_report){
+detailed_pn_matrix_for_report %>%
+  inner_join(manifest %>% mutate(barcode = paste0(BC1,BC2)) %>% select(-BC1,-BC2)) %>% 
+  full_join(control_for_report %>% select(Owner_Sample_ID,Control_Code)) %>%
+  select(barcode,Owner_Sample_ID,ASIC_Low,ASIC_Med,ASIC_High,B2M_L,B2M_S,ESIC_High,ESIC_Low,ESIC_Med,Assay_Plate_Code,Assay_SIC,Ext_SIC,human_control) %>%
+  distinct() %>%
+  mutate(num = 1) %>%
+  mutate(plate_sum = sum(num)) %>%
+  mutate(ASIC_num = ifelse(Assay_SIC == "pass",1,0)) %>%
+  mutate(ESIC_num = ifelse(Ext_SIC == "pass",1,0)) %>% 
+  drop_na() %>%
+  mutate(plate_ASIC = sum(ASIC_num),plate_ESIC = sum(ESIC_num)) %>% 
+  mutate(plate_ASIC_perc = scales::percent(plate_ASIC/plate_sum), plate_ESIC_perc = scales::percent(plate_ESIC/plate_sum)) %>%
+  group_by(Assay_Plate_Code) %>%
+  mutate(num = sum(num)) %>%
+  mutate(ASIC_num = sum(ASIC_num), ESIC_num = sum(ESIC_num)) %>%
+  mutate(Perc_ASIC_passed = scales::percent(ASIC_num/num), Perc_ESIC_passed = scales::percent(ESIC_num/num)) %>%
+  ungroup() %>%
+  select(Assay_Plate_Code,Perc_ASIC_passed,Perc_ESIC_passed, plate_ASIC_perc,plate_ESIC_perc) %>%
+  distinct() %>%
+  add_row(Assay_Plate_Code = "All_plates",Perc_ASIC_passed = unique(.$plate_ASIC_perc), Perc_ESIC_passed = unique(.$plate_ESIC_perc))%>%
+  select(Assay_Plate_Code,Perc_ASIC_passed,Perc_ESIC_passed) -> control_df1
+
+
+#Doing the B2M calculation seperately since B2M is being calculated only in samples while other 
+#internal controls are being calculated on controls and samples.
+
+control_df2 = detailed_pn_matrix_for_report %>%
+  inner_join(manifest %>% mutate(barcode = paste0(BC1,BC2)) %>% select(-BC1,-BC2)) %>% 
+  full_join(control_for_report %>% select(Owner_Sample_ID,Control_Code)) %>%
+  filter(is.na(Control_Code)) %>%
+  select(barcode,Owner_Sample_ID,human_control,Assay_Plate_Code) %>%
+  distinct() %>%
+  mutate(B2M_num = ifelse(human_control == "pass",1,0)) %>%
+  mutate(num = 1) %>%
+  mutate(plate_sum = sum(num)) %>%
+  mutate(plate_B2M = sum(B2M_num)) %>%
+  mutate(plate_B2M_perc = scales::percent(plate_B2M/plate_sum)) %>%
+  group_by(Assay_Plate_Code) %>%
+  mutate(num = sum(num)) %>%
+  mutate(B2M_num = sum(B2M_num)) %>%
+  mutate(Perc_B2M_passed = scales::percent(B2M_num/num)) %>%
+  ungroup() %>%
+  select(Assay_Plate_Code,Perc_B2M_passed, plate_B2M_perc) %>%
+  distinct() %>% 
+  drop_na() %>%
+  add_row(Assay_Plate_Code = "All_plates", Perc_B2M_passed  = unique(.$plate_B2M_perc)) %>%
+  inner_join(control_df1) %>%
+  select(-plate_B2M_perc)
+
+
+ panderOptions("table.split.cells", 4)
+ 
+ control_df2 %>%
+   pandoc.table(style = "multiline",
+                caption = "Control Summary")
+ 
+
+
+}
+
+
+
+
