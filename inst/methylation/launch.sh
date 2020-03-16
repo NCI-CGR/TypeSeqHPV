e5e3fbd7702a74523e44cffb4488cd6647502715
Shastry <shastrya2@nci-02134343-ml.nih.gov>
2020-03-16 15:50:45 -0400

    Added a missing '\' which was leaving out all other options

--- inst/methylation/launch.sh
+++ inst/methylation/launch.sh
@@ -13,7 +13,7 @@ docker run -i -v $(pwd):/mnt -v /mnt:/user_files \
         --is_torrent_server yes \
         --config_file config_file.csv \
         --barcode_file barcodes.csv \
-        --control_definitions control_defs.csv
+        --control_definitions control_defs.csv \
         --cores 22 \
         --hotspot_vcf TS2-T52_v1-HOTSPOT.hotspot.vcf \
         --manifest manifest.csv \
