023ec6884222e14f7b04612d87b9146c90cdf217
Shastry <shastrya2@nci-02134343-ml.nih.gov>
2020-04-14 17:52:49 -0400

    Fixing accidental push to this file

--- R/adam_demux.R
+++ R/adam_demux.R
@@ -3,11 +3,11 @@ adam_demux <- function(user_files, ram, cores){
     require(dplyr)
     require(fs)
 
-  #  system(paste0("/home/adam/bin/adam-shell --driver-memory ", ram," --driver-cores ",cores," -i /TypeSeqHPV/inst/methylation/demux_3prime_barcode_adam.scala"))
+    system(paste0("/home/adam/bin/adam-shell --driver-memory ", ram," --driver-cores ",cores," -i /TypeSeqHPV/inst/methylation/demux_3prime_barcode_adam.scala"))
 
-    system(paste0("samtools index *.bam"), wait = TRUE)
+    system(paste0("samtools index demux_reads.bam"), wait = TRUE)
 
-#    system("cp read_summary_df.csv/*csv read_summary.csv")
+    system("cp read_summary_df.csv/*csv read_summary.csv")
 
     dir_ls("./", recursive = T, glob = "*.csv") %>%
         map_df(as_tibble)  %>%
