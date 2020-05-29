8dc3e58e11414207314f7465f6cb187ca29306b8
Ashastry2 <amulyashastry@gmail.com>
2020-05-29 18:37:29 -0400

    Fixed the summary count to include all the samples correctly.

--- R/sample_summary.R
+++ R/sample_summary.R
@@ -11,8 +11,12 @@ sample_summary <- function(df){
         mutate(numSamplesTested = n()) %>%
         group_by(Project, numSamplesTested, human_control) %>%
         summarize(count = n()) %>%
-        ungroup() %>%
-        spread(human_control, count, fill=0) %>%
+        transform(human_control = as.character(human_control)) %>%   
+        mutate(human_control = ifelse(str_detect(human_control,'pass'),"pass",human_control)) %>%  
+        ungroup() %>% group_by(Project,human_control) %>% 
+        mutate(sum = sum(count)) %>% select(-count) %>% 
+        distinct() %>% 
+        spread(human_control, sum, fill=0) %>%
         select(Project_ID = Project, `Number Samples Tested` = numSamplesTested, everything()) %>%
         mutate(`Number Passed` = ifelse("pass" %in% colnames(.),pass,"")) %>%
         mutate(`Number Failed` = ifelse("failed_to_amplify" %in% colnames(.),failed_to_amplify,"")) %>%
