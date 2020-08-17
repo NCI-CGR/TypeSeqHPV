109a437a828a90a96780fc65a4ac7e58d5732260
Ashastry2 <amulyashastry@gmail.com>
2020-08-17 19:48:05 -0400

    Sorting platemaps Adding labels

--- R/plate_map.R
+++ R/plate_map.R
@@ -49,18 +49,19 @@ for (i in unique(plate_data$Assay_Plate_Code)) {
   plate_data %>%
     filter(Assay_Plate_Code == i)  -> data
   data %>% 
-    full_join(empty_wells %>% transform(colnum = as.character(colnum))) %>%
+    full_join(empty_wells %>% transform(colnum = as.factor(colnum), rownum = as.factor(rownum))) %>%
     mutate(color = ifelse(is.na(color),"empty",color)) %>%
-    mutate(Control_Code = ifelse(is.na(Control_Code),"empty",Control_Code)) %>%
-    mutate(colnum = fct_reorder(colnum, as.numeric(colnum))) -> data
+    mutate(Control_Code = ifelse(is.na(Control_Code),"empty",Control_Code)) -> data
+   # mutate(colnum = fct_reorder(colnum, desc(as.numeric(colnum)))) 
   
   data = data[order(as.numeric(as.character(data$colnum))),]
   cols = c("0/3_present" = "red","1/3_present" = "yellow","2/3_present"="orange","3/3_present"="green","empty"="grey")
-  plot = ggplot(data, aes(y = factor(rownum),x = factor(colnum),shape = Control_Code)) + 
+  plot = ggplot(data, aes(y = fct_reorder(rownum,desc(rownum)),x = fct_reorder(colnum,sort(as.numeric(data$colnum))),shape = Control_Code)) + 
     geom_point(aes(colour = color), size =12) + 
     scale_shape_manual(values = c("empty"=16,"control"=17,"sample"=16), limits = c("empty","control","sample"))+
     scale_color_manual(values = cols) + theme_bw() +
-    labs(x= i, y = "TypeSeqHPV_plate_data")  
+    labs(x= i, y = "ASIC_plate_map")+
+    ggtitle("ASIC plate map")
   
  # plot_list[[i]] = plot
   print(plot)
@@ -104,18 +105,18 @@ for (i in unique(plate_data$Assay_Plate_Code)) {
   plate_data %>%
     filter(Assay_Plate_Code == i)  -> data
   data %>%
-    full_join(empty_wells %>% transform(colnum = as.character(colnum))) %>%
+    full_join(empty_wells %>% transform(colnum = as.character(colnum), rownum = as.factor(rownum))) %>%
     mutate(color = ifelse(is.na(color),"empty",color)) %>%
-    mutate(Control_Code = ifelse(is.na(Control_Code),"empty",Control_Code)) %>%
-    mutate(colnum = fct_reorder(colnum, as.numeric(colnum))) -> data
+    mutate(Control_Code = ifelse(is.na(Control_Code),"empty",Control_Code)) -> data
  
   data = data[order(as.numeric(as.character(data$colnum))),]
   cols = c("0/2_present"="red","1/2_present"="yellow","2/2_present" = "green","empty"="grey")
-  plot = ggplot(data, aes(x = colnum,y = rownum,shape = Control_Code)) + 
+  plot = ggplot(data, aes(x = fct_reorder(colnum,sort(as.numeric(data$colnum))),y = fct_reorder(rownum,desc(rownum)),shape = Control_Code)) + 
     geom_point(aes(colour = color), size =12) +
     scale_shape_manual(values = c("empty"=16,"control"=17,"sample"=16), limit = c("empty","control","sample"), drop = F) +
     scale_color_manual(values = cols, limits = c("0/2_present","1/2_present","2/2_present","empty"),  drop = F) +
-    labs(x= i, y = "TypeSeqHPV_plate_data")
+    labs(x= i, y = "TypeSeqHPV_plate_data")+
+    ggtitle("B2M/Human_control plate map")
     
 
   print(plot)
@@ -164,23 +165,23 @@ for (i in unique(plate_data$Assay_Plate_Code)) {
     filter(Assay_Plate_Code == i)  -> data
   
   data %>% 
-    full_join(empty_wells %>% transform(colnum = as.character(colnum))) %>% 
+    full_join(empty_wells %>% transform(colnum = as.character(colnum), rownum = as.factor(rownum))) %>% 
     mutate(color = ifelse(is.na(color),"empty",color)) %>%
     mutate(Owner_Sample_ID = ifelse(is.na(Owner_Sample_ID), "empty",Owner_Sample_ID)) %>%
     mutate(control_result = ifelse(is.na(control_result),"empty",control_result)) %>%
     mutate(Control_type =ifelse(is.na(Control_type), "empty",Control_type))%>%
     mutate(Control_code = ifelse(is.na(Control_code),"empty",Control_code)) %>%
-    mutate(Control_code = ifelse(Control_code != "sample" & Control_code != "empty","control",Control_code)) %>%
-    mutate(colnum = fct_reorder(colnum, desc(as.numeric(colnum)))) -> data
+    mutate(Control_code = ifelse(Control_code != "sample" & Control_code != "empty","control",Control_code)) -> data
   
   data = data[order(as.numeric(as.character(data$colnum))),]
   cols = c("pos_pass"='green',"pos_fail"='red',"neg_pass"='blue',"neg_fail"='yellow',"sample" ='white',"empty" ='grey')
-  plot = ggplot(data, aes(x = colnum,y = rownum, shape = Control_code)) + 
+  plot = ggplot(data, aes(x = fct_reorder(colnum,sort(as.numeric(data$colnum))),y = fct_reorder(rownum,desc(rownum)), shape = Control_code)) + 
     geom_point(aes(colour = color), size =12) +
     scale_shape_manual(name = "Shape", values = c("sample"= 16,"control" = 17,"empty"= 16),limits = c("sample","control","empty"), drop = FALSE) +
     scale_color_manual(name = "Color",values = cols, limits = c("pos_pass","pos_fail","neg_pass","neg_fail","sample","empty")) +
     labs(x= i, y = "TypeSeqHPV_plate_data") +
-    scale_x_discrete(limits = rev(levels(data$colnum))) 
+    scale_x_discrete(limits = rev(levels(data$colnum))) +
+    ggtitle("All batch control plate map")
   
  # plot_list[[i]] = plot
   
