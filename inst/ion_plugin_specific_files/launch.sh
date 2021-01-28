68088eb40a91c9ab3d4caaeb20d2155d39fa2e66
Ashastry2 <amulyashastry@gmail.com>
2021-01-28 17:49:55 -0500

    Update to new docker for controls

--- inst/ion_plugin_specific_files/launch.sh
+++ inst/ion_plugin_specific_files/launch.sh
@@ -9,12 +9,12 @@ ln ../../*.bam ./
 
 docker run -i -v $(pwd):/mnt \
 -v /results/plugins/scratch/TypeSeqHPV-TSv1/:/report_dir \
-cgrlab/typeseqhpv:final_190104 Rscript /TypeSeqHPV/workflows/ion_workflow.R \
+cgrlab/typeseqhpv:version1_controls_210128 Rscript /TypeSeqHPV/workflows/ion_workflow.R \
 --pos_neg_filtering_criteria /TypeSeqHPV/docs/Ion/2017-06-11_Pos-Neg_matrix_filtering_criteria_RefTable_v3.txt \
 --scaling_table /TypeSeqHPV/docs/Ion/2017-11-24_TypeSeqer_Filtering_Scaling_Table_v2.csv \
 --parameter_file /TypeSeqHPV/docs/Ion/hpv_types_MQ_min_max_len_filters_JUNE2017_30-10bpLen_v6.txt \
 --lineage_reference /TypeSeqHPV/docs/Ion/20180121_lineage_reference_table_and_filters_2.csv \
---barcode_list /TypeSeqHPV/docs/Ion/barcodeList_v1.txt \
+--barcode_list /TypeSeqHPV/docs/Ion/barcodeList_TypingV1_v2-barcodes.csv \
 --bam_files_dir /mnt/ \
 --start_plugin /mnt/startplugin.json \
 --custom_groups /mnt/report_grouping.csv \
